6a1785950f8ac26c69be35788dbde9bff97728b0
ghazalb76 <ghazal.b93@gmail.com>
2019-03-01 19:20:57 +0330

    Add discrete part

--- 4-3.py
+++ 4-3.py
@@ -13,5 +13,12 @@ y2 = np.asarray([2*e**(n) for n in list(x)])
 y = y1*y2
 plt.plot(x,y)
 
+#discrete
+x = np.arange(-5, 5, 1)
+y1 = np.cos(np.pi/3*x+np.pi/4)
+y2 = np.asarray([2*e**(n) for n in list(x)])
+y = y1*y2
+plt.stem(x,y)
+
 #show
 plt.show()
