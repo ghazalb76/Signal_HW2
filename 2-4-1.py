462c3025b6fa6b5020b086b10dbb723e21ad339d
ghazalb76 <ghazal.b93@gmail.com>
2019-03-01 20:52:51 +0330

    Implement fourth part of question 4

---
+++ 2-4-1.py
@@ -0,0 +1,20 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+#continuous
+x = np.arange(-5, 5, 0.01)
+y1 = np.cos(9*x)
+y2 = np.cos(2*np.pi*x)
+y = y1+y2
+plt.plot(x,y)
+
+#discrete
+x = np.arange(-5, 5, 1)
+y1 = np.cos(9*x)
+y2 = np.cos(2*np.pi*x)
+y = y1+y2
+plt.stem(x,y)
+
+#show
+plt.show()
